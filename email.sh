e61d36c2e53ccf92a37b9fc256cbf1cb5fc505f5
Sahanamahadev29 <113870446+Sahanamahadev29@users.noreply.github.com>
2022-10-15 20:33:02 +0530

    Update email.sh

--- email.sh
+++ email.sh
@@ -3,3 +3,4 @@ perc=`df -h . | awk -F " " 'NR==2{print $(NF-1)}' | sed 's/%/ /g'`
 if [ $perc -ge 20 ]; then
 echo "memory reached thershould value 30%" | mail -s "Disk usage" nammu2443@gmail.com
 fi
+#notning i am commiting
